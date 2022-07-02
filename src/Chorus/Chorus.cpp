2e7a9277233eea3e2d10d46f59a8697b353f62e1
John Kasun <johnkasun@live.com>
2022-07-02 09:31:35 -0400

    Convert to setters and getters

--- src/Chorus/Chorus.cpp
+++ src/Chorus/Chorus.cpp
@@ -30,6 +30,36 @@ Error_t Chorus::reset()
 	}
 }
 
+Error_t Chorus::setDepth(float newDepth)
+{
+	return Error_t();
+}
+
+Error_t Chorus::setSpeed(float newSpeed)
+{
+	return Error_t();
+}
+
+Error_t Chorus::setShape(Chorus::Shape newShape)
+{
+	return Error_t();
+}
+
+float Chorus::getDepth() const
+{
+	return 0.0f;
+}
+
+float Chorus::getSpeed() const
+{
+	return 0.0f;
+}
+
+Chorus::Shape Chorus::getShape() const
+{
+	return Chorus::Shape();
+}
+
 
 Error_t Chorus::process(const float const* inputBuffer, float const* outputBuffer, const int numSamples)
 {
