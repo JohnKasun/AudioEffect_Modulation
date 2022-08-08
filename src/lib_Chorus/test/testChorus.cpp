1e7c9151a5a22339a891e3ba050548f18ec9b7f9
John Kasun <johnkasun@live.com>
2022-08-08 17:28:17 -0400

    Technically fixed ZeroSpeed test but am confused on logic

--- src/lib_Chorus/test/testChorus.cpp
+++ src/lib_Chorus/test/testChorus.cpp
@@ -43,7 +43,7 @@ class ChorusTestSuite : public ::testing::Test {
 TEST_F(ChorusTestSuite, ZeroDepth) {
   mSpeedParam = 1;
   CSynthesis::generateSine(mInputBuffer.get(), 440, mSampleRate, mNumSamples);
-  auto delayInSamp = int{CUtil::float2int<int>((Chorus::DelayInMs / 2.0f + Chorus::MaxDepthInMs) * mSampleRate / 1000)};
+  auto delayInSamp = int{CUtil::float2int<int>((Chorus::DelayInMs + Chorus::MaxDepthInMs / 2.0f) * mSampleRate / 1000)};
   CVectorFloat::copy(mGroundBuffer.get(), mInputBuffer.get(), mNumSamples);
   CVectorFloat::add_I(mGroundBuffer.get() + delayInSamp, mInputBuffer.get(), mNumSamples - delayInSamp);
   CVectorFloat::mulC_I(mGroundBuffer.get(), 0.5, mNumSamples);
@@ -64,15 +64,20 @@ TEST_F(ChorusTestSuite, ZeroDepth) {
 TEST_F(ChorusTestSuite, ZeroSpeed) {
   mDepthParam = 20.0f;
   CSynthesis::generateSine(mInputBuffer.get(), 440, mSampleRate, mNumSamples);
+  auto delayInSamp = int{CUtil::float2int<int>(Chorus::DelayInMs * mSampleRate / 1000)};
   CVectorFloat::copy(mGroundBuffer.get(), mInputBuffer.get(), mNumSamples);
-  CVectorFloat::add_I(mGroundBuffer.get(), mInputBuffer.get(), mNumSamples);
+  CVectorFloat::add_I(mGroundBuffer.get() + delayInSamp, mInputBuffer.get(), mNumSamples - delayInSamp);
   CVectorFloat::mulC_I(mGroundBuffer.get(), 0.5, mNumSamples);
-  mChorus->setDepth(mDepthParam);
-  mChorus->setSpeed(mSpeedParam);
-  mChorus->setShape(mShapeParam);
-  mChorus->setNumVoices(mVoicesParam);
-  mChorus->setGain(mGainParam);
-  mChorus->setMix(mMixParam);
-  mChorus->process(mInputBuffer.get(), mOutputBuffer.get(), mNumSamples);
+  try {
+    mChorus->setDepth(mDepthParam);
+    mChorus->setSpeed(mSpeedParam);
+    mChorus->setShape(mShapeParam);
+    mChorus->setNumVoices(mVoicesParam);
+    mChorus->setGain(mGainParam);
+    mChorus->setMix(mMixParam);
+    mChorus->process(mInputBuffer.get(), mOutputBuffer.get(), mNumSamples);
+  } catch (Exception& ex) {
+    FAIL();
+  }
   GTestUtil::compare(mOutputBuffer.get(), mGroundBuffer.get(), mNumSamples);
 }
